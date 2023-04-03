739439431b11ba74b30836fdd164e3242c0ead7d
Aidan Enseleit <aidanenseleit98@gmail.com>
2023-04-03 14:27:00 +1000

    Latest

--- preload.js
+++ preload.js
@@ -1,72 +1,70 @@
-const Sentry = require("@sentry/electron");
-const Tracing = require("@sentry/tracing");
-const version = require('./package.json').version;
-const { contextBridge, ipcRenderer } = require('electron')
+//Const Sentry = require('@sentry/electron');
+//Const Tracing = require('@sentry/tracing');
+//Const version = require('./package.json').version;
+const {contextBridge, ipcRenderer} = require('electron');
 
-function initSentry() {
-    if(process.argv[2] === '--dev' && !process.argv.includes("--sentry")) return;
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        release: "youtube-dl-gui@" + version,
-        sendDefaultPii: true,
-        integrations: [new Tracing.Integrations.BrowserTracing()],
-        tracesSampleRate: process.argv[2] === '--dev' ? 1.0 : 0.01,
-        environment: process.argv[2] === '--dev' ? "development" : "production",
-        autoSessionTracking: true
-    });
-}
+//
+//Function initSentry() {
+//If (process.argv[2] === '--dev' && !process.argv.includes('--sentry')) return;
+//Sentry.init({
+//Dsn: process.env.SENTRY_DSN,
+//Release: 'youtube-dl-gui@' + version,
+//SendDefaultPii: true,
+//Integrations: [new Tracing.Integrations.BrowserTracing()],
+//TracesSampleRate: process.argv[2] === '--dev' ? 1.0 : 0.01,
+//Environment: process.argv[2] === '--dev' ? 'development' : 'production',
+//AutoSessionTracking: true,
+//});
+//}
 
-initSentry();
+//InitSentry();
 
-contextBridge.exposeInMainWorld(
-    "main",
-    {
-        invoke: async (channel, data) => {
-            let validChannels = [
-                "platform",
-                "messageBox",
-                "errorReport",
-                "titlebarClick",
-                "openInputMenu",
-                "openCopyMenu",
-                "settingsAction",
-                "videoAction",
-                "cookieFile",
-                "downloadFolder",
-                "installUpdate",
-                "iconProgress",
-                "theme",
-                "restoreTaskList",
-                "getDoneActions",
-                "setDoneAction",
-                "getSubtitles",
-                "getSelectedSubtitles",
-                "getLog",
-                "saveLog"
-            ];
-            if (validChannels.includes(channel)) {
-                return await ipcRenderer.invoke(channel, data);
-            }
-        },
-        receive: (channel, cb) => {
-            let validChannels = [
-                "log",
-                "error",
-                "toast",
-                "maximized",
-                "videoAction",
-                "updateGlobalButtons",
-                "updateLinkPlaceholder",
-                "totalSize",
-                "binaryLock",
-                "addShortcut",
-                "downloadShortcut"
-            ];
-            if (validChannels.includes(channel)) {
-                ipcRenderer.on(channel, (event, arg) => {
-                    cb(arg)
-                });
-            }
-        }
+contextBridge.exposeInMainWorld('main', {
+  invoke: async (channel, data) => {
+    let validChannels = [
+      'platform',
+      'messageBox',
+      'errorReport',
+      'titlebarClick',
+      'openInputMenu',
+      'openCopyMenu',
+      'settingsAction',
+      'videoAction',
+      'cookieFile',
+      'downloadFolder',
+      'installUpdate',
+      'iconProgress',
+      'theme',
+      'restoreTaskList',
+      'getDoneActions',
+      'setDoneAction',
+      'getSubtitles',
+      'getSelectedSubtitles',
+      'getLog',
+      'saveLog',
+    ];
+    if (validChannels.includes(channel)) {
+      return await ipcRenderer.invoke(channel, data);
     }
-);
+  },
+  receive: (channel, cb) => {
+    let validChannels = [
+      'log',
+      'error',
+      'toast',
+      'maximized',
+      'videoAction',
+      'updateGlobalButtons',
+      'updateLinkPlaceholder',
+      'totalSize',
+      'binaryLock',
+      'addShortcut',
+      'downloadShortcut',
+    ];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, arg) => {
+        cb(arg);
+      });
+    }
+  },
+});
